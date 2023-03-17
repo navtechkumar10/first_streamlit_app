cbee2eecfbe5ec2fe774d8f6f7107053941f4494
navtechkumar10 <126998677+navtechkumar10@users.noreply.github.com>
2023-03-17 11:58:40 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,12 @@ def get_fruitvice_data(this_fruit_choice):
    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
    return fruityvice_normalized
 
+def get_fruit_load_list():
+ with my_cnx.cursor() as my_cur:
+   my_cur.execute("select * from fruit_load_list")
+   my_data_rows = my_cur.fetchall()
+   return my_data_rows
+ 
 streamlit.header('Fruityvice Fruit Advice!')
 try:
  #pass fruit name to api
@@ -56,10 +62,15 @@ streamlit.stop()
 
 
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
+
+
+#Add Streamlit button
+if streamlit.button("Get Fruit load list"):
+ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+ my_data_rows = get_fruit_load_list()
+ streamlit.dataframe(my_data_rows)
+
+
 streamlit.text("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
